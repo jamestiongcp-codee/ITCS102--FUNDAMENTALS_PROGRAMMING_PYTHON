de65ba65995303646fe1d0862b2cf733cea4aa57
jamestiongcp-codee <jamestiongco75@gmail.com>
2025-09-18 07:59:51 +0800

    Add files via upload

---
+++ Code_challenge9.py
@@ -0,0 +1,8 @@
+print("COUNTDOWN TIMER SIMULATOR")
+num = eval(input("Enter the starting number for countdown:"))
+print("\nCountdown started:")
+for j in range (num,0,-1):
+  print(j)
+print("Liftoff!!!")
+
+        
