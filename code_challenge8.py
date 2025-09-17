b82a0946bf377f662c42e8000e641ce5b7516a0c
jamestiongcp-codee <jamestiongco75@gmail.com>
2025-09-17 08:36:10 +0800

    Add files via upload

---
+++ code_challenge8.py
@@ -0,0 +1,12 @@
+result = 1
+print("Multiplication Table Maker")
+num = eval(input("Enter a number:" ))
+print("\nMultiplication Table for",num,":")
+for j in range(1,11,1):
+        result = num * j
+        print(num,"×", j,"=", result) 
+        
+                        
+
+        
+        
---
+++ code_challenge9.py
@@ -0,0 +1,6 @@
+print("COUNTDOWN TIMER SIMULATOR")
+num = eval(input("Enter the starting number for countdown:"))
+print("\nCountdown started:")
+for j in range (num,0,-1):
+    print(j)
+print("Liftoff!!!")
