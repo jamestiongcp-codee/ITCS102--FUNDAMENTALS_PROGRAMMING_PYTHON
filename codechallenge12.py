2a13b6fd502d1a29e753ca85d6744f3c0ef978d5
jamestiongcp-codee <jamestiongco75@gmail.com>
2025-10-12 19:37:56 +0800

    Add files via upload

---
+++ codechallenge12.py
@@ -0,0 +1,9 @@
+for j in range (1,7,1):
+    for jj in range (6,j,-1):
+        print(" ",end=" ")
+    for jjj in range(j,0,-1):
+        print(jjj, end=" ")
+    for jjjj in range(2,j+1,1):
+        print(jjjj, end=" ")
+    print()
+       
---
+++ codechallenge13.py
@@ -0,0 +1,75 @@
+result= input("Wanna see a Christmas tree?, (yes/no)")
+
+if result.lower()== "yes":
+    print("Here ya go:")
+    print("\n\t  *")
+    print("\t*","*","*")
+    print("\t  *")
+
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for b in range (1,8,1):
+        for bb in range (3,0,-1):
+            print(" ",end=" ")
+        for bbb in range (1,6,1):
+            print("*",end=" ")
+        print()
+ 
+else: 
+    print("I'll still let you see it:")
+    print("\n\t  *")
+    print("\t*","*","*")
+    print("\t  *")
+
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for j in range (1,7,1):
+        for jj in range (6,j,-1):
+            print(" ",end=" ")
+        for jjj in range(j,0,-1):
+            print("*", end=" ")
+        for jjjj in range(2,j+1,1):
+            print("*", end=" ")
+        print()
+    for b in range (1,8,1):
+        for bb in range (3,0,-1):
+            print(" ",end=" ")
+        for bbb in range (1,6,1):
+            print("*",end=" ")
+        print()
