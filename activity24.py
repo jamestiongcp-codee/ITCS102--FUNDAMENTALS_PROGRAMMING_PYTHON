5bcc2e0431c3c8643cf20b3b8a27907ab40005fa
jamestiongcp-codee <jamestiongco75@gmail.com>
2025-11-26 10:53:52 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,19 @@
+def greeter(name):
+    print(f"Hi {name}, how do you do?")
+
+greeter('james')
+greeter('kent')
+greeter('cruz')
+greeter('acie')
+greeter('sumagop')
+
+def summation(x):
+    sum = 0
+    for y in range(x,0,-1):
+        sum += y
+    print(f"The summation of {x} is {sum}")
+    
+summation(14)
+summation(13)
+
+
---
+++ activity24_1.py
@@ -0,0 +1,6 @@
+from activity24 import greeter, summation
+greeter('nerio')
+greeter('jessa mae')
+
+summation(23)
+summation(90)
