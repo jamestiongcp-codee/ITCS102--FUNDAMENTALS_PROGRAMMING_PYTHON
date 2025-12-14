1378057970e94765c639c456211573ff65208f62
jamestiongcp-codee <jamestiongco75@gmail.com>
2025-12-14 18:11:49 +0800

    Delete operators.py

--- operators.py
+++
@@ -1,4 +0,0 @@
-def oper():
-    print("What would you like to know under this topic?-->")
-    print("A - Arithmetic Operators\nB - Assignment Operators\nC - Relational Operators\nD - Logical Operators")
-    
